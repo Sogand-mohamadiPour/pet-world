a0405b6315f3a227db187b0e46679e1e016d2781
Sogand-mohamadiPour <sogandmohamadipour7@gmail.com>
2025-04-20 18:50:27 +0330

    compliting
    main page UI almost done

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 // load page
 let change_b = false;
-// ------
+// ------ change theme
 function change_theme() {
     if (change_b == false) {
-        document.getElementById("moons").src = "assets/pic/sun.png";
-        document.getElementById("moon").src = "assets/pic/sun.png";
-        document.getElementById("inpt").style.backgroundColor='#cccccc';
-        document.getElementById("btn").style.backgroundColor='#cccccc';
-        document.getElementById("inpt").style.color="white";
-        document.querySelector("#offcanvasNavbar").style.backgroundColor="#d69ade";
-        document.querySelector("body").style.backgroundColor="#ccc5b9";
+        document.getElementById("moons").src = "assets/pic/sun.png"; // moon desktop
+        document.getElementById("moon").src = "assets/pic/sun.png"; // moon mobile
+        document.getElementById("inpt").style.backgroundColor='#cccccc'; // search bar
+        document.getElementById("btn").style.backgroundColor='#cccccc'; // search bar btn
+        document.getElementById("inpt").style.color="white"; // search bar font
+        document.querySelector("#offcanvasNavbar").style.backgroundColor="#d69ade"; // mobile menu bg color
+        document.querySelector("body").style.backgroundColor="#BBBCB6"; // body bg
+        document.querySelector(".nv").style.color="black"; // pet word color
+        document.querySelectorAll(".icons")[0].style.color="black"; // icons color
+        document.querySelectorAll(".icons")[1].style.color="black"; // icons color
         change_b = true;
     }
     else {
@@ -20,6 +23,9 @@ function change_theme() {
         document.getElementById("inpt").style.color="black";
         document.querySelector("#offcanvasNavbar").style.backgroundColor="white";
         document.querySelector("body").style.backgroundColor="white";
+        document.querySelector(".nv").style.color="white"; // pet word color
+        document.querySelectorAll(".icons")[0].style.color="white"; // icons color
+        document.querySelectorAll(".icons")[1].style.color="white"; // icons color
         change_b = false;
     }
 }
