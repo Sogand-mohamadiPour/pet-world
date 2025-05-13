4125ca53d33ab63b51048102743425da3b1e9036
Sogand-mohamadiPour <sogandmohamadipour7@gmail.com>
2025-05-13 23:04:37 +0330

    slider
    changed slider to js slider and some small changes as well

--- index.js
+++ index.js
@@ -5,31 +5,31 @@ let change_b = false;
 function change_theme() {
   if (change_b == false) {
     document.getElementById("moons").src = "assets/pic/sun.png"; // moon desktop
-        document.getElementById("moon").src = "assets/pic/sun.png"; // moon mobile
-        document.getElementById("inpt").style.backgroundColor = '#cccccc'; // search bar
-        document.getElementById("btn").style.backgroundColor = '#cccccc'; // search bar btn
-        document.getElementById("inpt").style.color = "white"; // search bar font
-        document.querySelector("#offcanvasNavbar").style.backgroundColor = "#d69ade"; // mobile menu bg color
-        document.querySelector("body").style.backgroundColor = "#BBBCB6"; // body bg
-        document.querySelector(".nv").style.color = "black"; // pet word color
-        document.querySelectorAll(".icons i")[0].style.color = "black"; // icons color
-        document.querySelectorAll(".icons i")[1].style.color = "black"; // icons color
-        document.querySelectorAll(".pet a")[1].style.setProperty("color", "black", "important"); // pet world color
-        change_b = true;
+    document.getElementById("moon").src = "assets/pic/sun.png"; // moon mobile
+    document.getElementById("inpt").style.backgroundColor = '#cccccc'; // search bar
+    document.getElementById("btn").style.backgroundColor = '#cccccc'; // search bar btn
+    document.getElementById("inpt").style.color = "white"; // search bar font
+    document.querySelector("#offcanvasNavbar").style.backgroundColor = "#d69ade"; // mobile menu bg color
+    document.querySelector("body").style.backgroundColor = "#BBBCB6"; // body bg
+    document.querySelector(".nv").style.color = "black"; // pet word color
+    document.querySelectorAll(".icons i")[0].style.color = "black"; // icons color
+    document.querySelectorAll(".icons i")[1].style.color = "black"; // icons color
+    document.querySelectorAll(".pet a")[1].style.setProperty("color", "black", "important"); // pet world color
+    change_b = true;
   }
   else {
     document.getElementById("moons").src = "assets/pic/moon_fill.png"; //moon desktop
-        document.getElementById("moon").src = "assets/pic/moon_fill.png"; // moon mobile
-        document.getElementById("inpt").style.backgroundColor = 'white'; //search bar
-        document.getElementById("btn").style.backgroundColor = '#F2EFE7'; //search bar btn
-        document.getElementById("inpt").style.color = "black"; // search bar font
-        document.querySelector("#offcanvasNavbar").style.backgroundColor = "white"; // mobile menu bg color
-        document.querySelector("body").style.backgroundColor = "white"; //body bg
-        document.querySelector(".nv").style.color = "white"; // pet word color
-        document.querySelectorAll(".icons i")[0].style.color = "white"; // icons color
-        document.querySelectorAll(".icons i")[1].style.color = "white"; // icons color
-        document.querySelectorAll(".pet a")[1].style.setProperty("color", "white", "important"); // pet world color
-        change_b = false;
+    document.getElementById("moon").src = "assets/pic/moon_fill.png"; // moon mobile
+    document.getElementById("inpt").style.backgroundColor = 'white'; //search bar
+    document.getElementById("btn").style.backgroundColor = '#F2EFE7'; //search bar btn
+    document.getElementById("inpt").style.color = "black"; // search bar font
+    document.querySelector("#offcanvasNavbar").style.backgroundColor = "white"; // mobile menu bg color
+    document.querySelector("body").style.backgroundColor = "white"; //body bg
+    document.querySelector(".nv").style.color = "white"; // pet word color
+    document.querySelectorAll(".icons i")[0].style.color = "white"; // icons color
+    document.querySelectorAll(".icons i")[1].style.color = "white"; // icons color
+    document.querySelectorAll(".pet a")[1].style.setProperty("color", "white", "important"); // pet world color
+    change_b = false;
   }
 }
 
@@ -56,3 +56,39 @@ window.addEventListener("load", () => {
     }
   }, 5000); // show 5 sec after load
 });
+
+// slider----------------------------------------------------------
+let arr = ['slider1.jpeg', 'slider222.jpg', 'slider3.png'];
+let i = 0;
+
+const prev = () => {
+  i = (i - 1 + arr.length) % arr.length; // back 
+  document.getElementById('slider-img').src = 'assets/pic/' + arr[i];
+}
+
+const next = () => {
+  i = (i + 1) % arr.length; // forward
+  document.getElementById('slider-img').src = 'assets/pic/' + arr[i];
+}
+
+// reserve inpt check--------------------------------------
+function check_name(e) {
+   let ch = e.keyCode;
+    if ((ch < 65 || ch > 90) && ch != 8) {
+        e.preventDefault();
+    }
+}
+
+function check_num(e) {
+  if (e.target.value.length >= 11 && e.keyCode !== 8) {
+    e.preventDefault();
+    return;
+  }
+
+  let ch = e.keyCode;
+  if ((ch < 48 || ch > 57) && (ch < 96 || ch > 105) && ch !== 8) {
+    e.preventDefault();
+  }
+}
+
+
